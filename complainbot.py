b1a2068b1ff4902398417f37389002eed6d5c4a4
Brutokvashino <106881300+Brutokvashino@users.noreply.github.com>
2023-01-08 16:57:07 -0500

    Create complainbot.py

---
+++ complainbot.py
@@ -0,0 +1,13 @@
+name = input("Well, who do we have here? ")
+print("Too bad, "+name+", Michael Scott would have made for a more interesting conversation :(")
+hours = int(input("How many hours did you sleep, "+name+"? "))
+modified = hours - 1
+print ("Well, "+name+", I only slept "+str(modified)+".")
+wish = input("What do want, "+name+"? ")
+print("I am SO tired of people saying \""+wish+"\"!\nThat sounds like a "+name+"-problem.")
+need = input("Just summarize what you need in a word and be realistic! ")
+print("I'm all out of "+need+". The "+need+"-warehouse burned down, we don't make "+need+" anymore, and the last "+need+" was supplied to the previous customer, "+name+"elina, so you're gonna have to make your own "+need+".")
+money = int(input("Please enter a tip amount for this paid service: $"))
+momoney = money + 500
+(input("So, you agree to pay $"+str(momoney)+".00? "))
+print("Okay, HAVE A GOOD ONE!")
